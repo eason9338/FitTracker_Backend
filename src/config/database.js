8f8e09278c12a61b8c2e33c3174000970778139f
eason9338 <9338akira@gmail.com>
2024-12-08 10:30:13 +0800

    deploy to mongodb atlas

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/FitTrack'
   try {
     await mongoose.connect(process.env.MONGODB_URL);
     console.log('MongoDB connected successfully');
